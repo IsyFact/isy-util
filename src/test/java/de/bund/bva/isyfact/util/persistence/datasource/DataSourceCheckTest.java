a099cbf887d4f31f09bc93bece449feef2483d5e
Niklas Schulpius <niklas.schulpius@seitenbau.com>
2026-02-18 09:33:48 +0100

    test: IFS-4890 refactor DataSourceCheckTest and DataSourceCheckRunnerTest

--- src/test/java/de/bund/bva/isyfact/util/persistence/datasource/DataSourceCheckTest.java
+++ src/test/java/de/bund/bva/isyfact/util/persistence/datasource/DataSourceCheckTest.java
@@ -1,9 +1,11 @@
 package de.bund.bva.isyfact.util.persistence.datasource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -12,101 +14,55 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.mockito.Mockito;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.mockito.Mockito;
 
 public class DataSourceCheckTest {
-    DataSource dataSource = Mockito.mock(DataSource.class);
-    Connection connection = Mockito.mock(Connection.class);
-    PreparedStatement statement = Mockito.mock(PreparedStatement.class);
-    ResultSet resultSet = Mockito.mock(ResultSet.class);
 
+    private DataSourceCheck dataSourceCheck;
+    private DataSource mockDataSource;
+    private ResultSet mockResultSet;
 
     @BeforeEach
-    public void setUp() throws SQLException {
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
-        when(statement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true);
+    void setUp() throws SQLException {
+        Connection mockConnection = Mockito.mock(Connection.class);
+        PreparedStatement mockStatement = Mockito.mock(PreparedStatement.class);
+
+        mockDataSource = Mockito.mock(DataSource.class);
+        mockResultSet = Mockito.mock(ResultSet.class);
+
+        when(mockDataSource.getConnection()).thenReturn(mockConnection);
+        when(mockConnection.prepareStatement(anyString())).thenReturn(mockStatement);
+        when(mockStatement.executeQuery()).thenReturn(mockResultSet);
+
+        dataSourceCheck = new DataSourceCheck();
     }
 
     @Test
-    public void testGetSchemaVersion() throws SQLException {
+    void testCheckSchemaVersionReturnsTrueWhenVersionMatches() throws SQLException {
         String expected = "47.11";
-        when(resultSet.getString(anyInt())).thenReturn(expected);
+        when(mockResultSet.next()).thenReturn(true);
+        when(mockResultSet.getString(anyInt())).thenReturn(expected);
 
-        DataSourceCheck check = new DataSourceCheck("SELECT version_nummer FROM m_schema_version WHERE version_nummer = ? AND status = 'gueltig'");
-        String version = check.getSchemaVersion(dataSource, "");
-        assertEquals(expected, version);
+        boolean result = dataSourceCheck.checkSchemaVersion(mockDataSource, expected);
+        assertTrue(result);
     }
 
     @Test
-    public void testGetSchemaVersionInvalid() throws SQLException {
-        String expected = "invalid";
-        when(resultSet.next()).thenReturn(false);
+    void testCheckSchemaVersionReturnsFalseWhenVersionMismatch() throws SQLException {
+        when(mockResultSet.next()).thenReturn(true);
+        when(mockResultSet.getString(anyInt())).thenReturn("47.11");
 
-        DataSourceCheck check = new DataSourceCheck("SELECT version_nummer FROM m_schema_version WHERE version_nummer = ? AND status = 'gueltig'");
-        String version = check.getSchemaVersion(dataSource, "");
-        assertEquals(expected, version);
+        boolean result = dataSourceCheck.checkSchemaVersion(mockDataSource, "0");
+        assertFalse(result);
     }
 
     @Test
-    public void testCheckSchemaVersionCriticalDataSourceTrue() throws SQLException {
-        String expected = "47.11";
-        when(resultSet.getString(anyInt())).thenReturn(expected);
+    void testCheckSchemaVersionThrowsRuntimeExceptionOnSQLException() throws SQLException {
+        when(mockResultSet.next()).thenReturn(true);
+        when(mockResultSet.getString(anyInt())).thenThrow(new SQLException("Testing an Exception"));
 
-        DataSourceCheck check = new DataSourceCheck("SELECT version_nummer FROM m_schema_version WHERE version_nummer = ? AND status = 'gueltig'");
-        boolean ok = check.checkSchemaVersionCriticalDataSource(dataSource, expected);
-        assertTrue(ok);
-    }
-
-    @Test
-    public void testCheckSchemaVersionCriticalDataSourceFalse() throws SQLException {
-        String expected = "47.11";
-        when(resultSet.getString(anyInt())).thenReturn(expected);
-
-        DataSourceCheck check = new DataSourceCheck("SELECT version_nummer FROM m_schema_version WHERE version_nummer = ? AND status = 'gueltig'");
-        boolean ok = check.checkSchemaVersionCriticalDataSource(dataSource, "0");
-        assertFalse(ok);
-    }
-
-
-    @Test
-    public void testCheckSchemaVersionNonCriticalDataSourceTrue() throws SQLException {
-        String expected = "47.11";
-        when(resultSet.getString(anyInt())).thenReturn(expected);
-
-        DataSourceCheck check = new DataSourceCheck("SELECT version_nummer FROM m_schema_version WHERE version_nummer = ? AND status = 'gueltig'");
-        boolean ok = check.checkSchemaVersionNonCriticalDataSource(dataSource, expected);
-        assertTrue(ok);
-    }
-
-    @Test
-    public void testCheckSchemaVersionNonCriticalDataSourceFalse() throws SQLException {
-        String expected = "47.11";
-        when(resultSet.getString(anyInt())).thenReturn(expected);
-
-        DataSourceCheck check = new DataSourceCheck("SELECT version_nummer FROM m_schema_version WHERE version_nummer = ? AND status = 'gueltig'");
-        boolean ok = check.checkSchemaVersionNonCriticalDataSource(dataSource, "0");
-        assertFalse(ok);
-    }
-
-    @Test
-    public void testCheckSchemaVersionCriticalDataSource_sqlException() throws SQLException {
-        when(resultSet.getString(anyInt())).thenThrow(new SQLException("Testing an Exception"));
-
-        DataSourceCheck check = new DataSourceCheck("SELECT version_nummer FROM m_schema_version WHERE version_nummer = ? AND status = 'gueltig'");
-        assertThrows(RuntimeException.class, () -> check.checkSchemaVersionCriticalDataSource(dataSource, "0"));
-    }
-
-    @Test
-    public void testCheckSchemaVersionNonCriticalDataSource_sqlException() throws SQLException {
-        when(resultSet.getString(anyInt())).thenThrow(new SQLException("Testing an Exception"));
-
-        DataSourceCheck check = new DataSourceCheck("SELECT version_nummer FROM m_schema_version WHERE version_nummer = ? AND status = 'gueltig'");
-        assertThrows(RuntimeException.class, () -> check.checkSchemaVersionNonCriticalDataSource(dataSource, "0"));
+        assertThrows(RuntimeException.class, () -> dataSourceCheck.checkSchemaVersion(mockDataSource, "0"));
     }
 }
